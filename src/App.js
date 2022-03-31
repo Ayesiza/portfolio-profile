73555435a20d34809a87b51a23e73247f7b0a8cb
Ayesiza Hawah <yesiha2001@gmail.com>
2022-03-31 17:09:31 +0300

    create list of projects

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About';
-import Home from './components/home/Home';
+
 import Layout from './components/Layout';
+import AboutMe from './Pages/AboutMe/AboutMe';
+import ContactMe from './Pages/ContactMe/ContactMe';
+import Home from './Pages/Home/Home';
+import Projects from './Pages/Projects/Projects';
 
 function App() {
   return (
     <Routes>
     <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-         
+          <Route path="about" element={<AboutMe />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="contact" element={<ContactMe />} />
 
-          
         </Route>
     </Routes>
   );
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, Outlet } from 'react-router-dom';
 
 const Layout = () => {
+  
     return (
         <div>
-          {/* A "layout route" is a good place to put markup you want to
-              share across all the pages on your site, like navigation. */}
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
+        
+ <nav className="navbar sticky-top navbar-expand-lg bg-light">
+    <div className="container">
+      <a className="navbar-brand" href="#">Ayesiza</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+   <i className="fas fa-bars"></i>
+  </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto w-100 justify-content-end">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="contact">ContactMe</Link>
               </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              
-            </ul>
-          </nav>
-    
-          <hr />
-    
-         
+              <li className="nav-item">
+                <Link className="nav-link" to="projects">Projects</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="#">Other</Link>
+                </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+      
           <Outlet />
         </div>
       );
